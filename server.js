4678be4e625387f884ddcafb9765f657d4d0c7bd
JoelGrayson <joel@joelgrayson.com>
2021-12-24 12:22:20 -0500

    Starting with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const path=require('path');
 const app=express();
+const mongoose=require('mongoose');
+
+let username='joel_user';
+let password='user_joel';
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.lzuaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -8,3 +13,4 @@ const port=process.env.PORT || 3001;
 app.listen(port, ()=>{
     console.log('listening on port '+port);
 });
+
