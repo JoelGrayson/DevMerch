52508f5578b23a0905610e5c71e5904989fd4628
JoelGrayson <joel@joelgrayson.com>
2021-12-25 07:08:48 -0500

    Added models and working on encrypting password

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,5 @@
 const router = require('express').Router();
 
-router.get('/usertest', (req, res) => {
-    console.log(`User test`);
-    res.send(`Success from usertest`);
-});
-
 router.post('/userposttest', (req, res) => {
     const username = req.body.username;
     console.log(username);
--- server.js
+++ server.js
@@ -3,25 +3,32 @@ require('dotenv').config();
 const express=require('express');
 const path=require('path');
 const app=express();
+app.use(express.json());
+
+//<Mongoose
 const mongoose=require('mongoose');
 
-app.use(express.json());
 let username=process.env.USERNAME;
 let password=process.env.PASSWORD;
 let mongoURL=`mongodb+srv://${username}:${password}@cluster0.lzuaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const usersRouter=require('./routes/users');
 
 mongoose.connect(mongoURL)
-    .then(()=>{
-        console.log(`DB Connection Success`);
-    })
-    .catch(e=>{
-        console.log(`There was an error`, e)
-    });
+.then(()=>{
+    console.log(`DB Connection Success`);
+})
+.catch(e=>{
+    console.log(`There was an error`, e)
+});
+// />
+
+const usersRouter=require('./routes/users');
+app.use('/api/users', usersRouter);
+
+const authRouter=require('./routes/auth');
+app.use('/api/auth', authRouter);
 
 
-app.use('/api/users', usersRouter)
 
 const port=process.env.PORT || 3001;
 app.listen(port, ()=>{
