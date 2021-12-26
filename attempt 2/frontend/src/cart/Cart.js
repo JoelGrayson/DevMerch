8f999885e7e3b9de99b312e658defb5718e378f9
JoelGrayson <joel@joelgrayson.com>
2021-12-25 20:49:19 -0500

    Cart page

--- attempt 2/frontend/src/cart/Cart.js
+++ attempt 2/frontend/src/cart/Cart.js
@@ -5,10 +5,19 @@ function Cart() {
     return (
         <div className="max-w-[1500px] mx-auto my-6">
             <h1 className='text-center'>Shopping Cart</h1>
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
+            <main className='flex max-w-[1500px] mx-auto justify-around'>
+                <div id='cart-left'>
+                    <CartItem />
+                    <CartItem />
+                    <CartItem />
+                    <CartItem />
+                </div>
+                <div id='cart-right' className='border-solid border-2 border-gray-300 p-5 rounded-xl shadow-xl h-fit'>
+                    <p>Subtotal of 2 items</p>
+                    <p className='border-b-2 border-b-solid border-b-gray-300 pb-2 mb-4'>$4.99</p>
+                    <button>Proceed to checkout</button>
+                </div>
+            </main>
         </div>
     )
 }
--- attempt 2/frontend/src/cart/component/CartItem.js
+++ attempt 2/frontend/src/cart/component/CartItem.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import './CartItem.css';
 
 function CartItem() {
     return (
-        <div>
-            Cart item
+        <div className='cartItem flex items-center border-solid border-2 border-black rounded-lg p-3 my-5'>
+            <Link to={`/product/${111}`}>
+                <img className='w-20 pr-5' src="https://i.ibb.co/VT68gRW/Vector-graphic-of-no-thumbnail-symbol.jpg" alt="Product" />
+            </Link>
+            <Link to={`/product/${111}`}>
+                <p>Product name</p>
+            </Link>
+            <div className='quantity'>
+                <span>Quantity: </span>
+                <select className='border-solid border-[1.5px] border-blue-800 rounded-md h-8'> 
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
+            </div>
+            <button className='delete bg-red-500'>
+                <i className='fas fa-trash'></i>
+            </button>
         </div>
     )
 }
