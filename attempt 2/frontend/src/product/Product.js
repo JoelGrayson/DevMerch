5f549c030572c4b888aa142242e18dab7df45912
JoelGrayson <joel@joelgrayson.com>
2021-12-26 15:41:05 -0500

    Add to cart correct quantity

--- attempt 2/frontend/src/product/Product.js
+++ attempt 2/frontend/src/product/Product.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './style.css';
 
 import { useParams } from 'react-router-dom';
@@ -11,6 +10,10 @@ function Product() {
     let params=useParams();
     let idFromParams=params.id; //id from react-router-dom
     
+    //Quantity
+    let [quantity, setQuantity]=useState(1);
+
+
     let [product, setProduct]=useState(null); //product info
     let [pageStatus, setPageStatus]=useState('Loading'); //shows loading... or product not found
 
@@ -52,14 +55,14 @@ function Product() {
                 <div className='product-right rounded-xl border-solid border-gray-400 border-2 shadow-lg w-[20%] p-6'> {/* Right (add to cart) */}
                     <div className="choose-size flex leading-[4] items-center">
                         <p>Quantity &emsp;</p>
-                        <select className='border-solid border-2 border-blue-800 rounded-xl h-8' id='foo'>
+                        <select className='border-solid border-2 border-blue-800 rounded-xl h-8' id='foo' value={quantity} onChange={e=>setQuantity(parseInt(e.target.value))}>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
                         </select>
                     </div>
-                    <button onClick={_=>{addToCart(product._id, 1)}}>Add to cart</button>
+                    <button onClick={_=>{addToCart(product._id, quantity)}}>Add to cart</button>
                 </div>
             </div>
         }</>);
