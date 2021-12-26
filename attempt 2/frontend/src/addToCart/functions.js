2083a6e9f73be3221731dff8491ae59e5490af34
JoelGrayson <joel@joelgrayson.com>
2021-12-26 15:02:50 -0500

    Show total cost in cart

--- attempt 2/frontend/src/addToCart/functions.js
+++ attempt 2/frontend/src/addToCart/functions.js
@@ -24,11 +24,11 @@ function removeFromCart(id) {
 
 function readCart() {
     let result=[];
-    for (let key in localStorage) {
-        if (key.slice(0, 9)==='cartItem:') { //part of localStorage that is cartItem
+    for (let objectKey in localStorage) {
+        if (objectKey.slice(0, 9)==='cartItem:') { //part of localStorage that is cartItem
             let item={
-                id: key.slice(9), //excludes cartItem: prefix
-                quantity: localStorage.getItem(key) //set valid key to valid value
+                id: objectKey.slice(9), //excludes cartItem: prefix
+                quantity: localStorage.getItem(objectKey) //set valid key to valid value
             };
             result.push(item);
         }
--- attempt 2/frontend/src/cart/Cart.js
+++ attempt 2/frontend/src/cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import CartItem from './component/CartItem';
 
 import { readCart} from '../addToCart/functions';
@@ -6,12 +6,31 @@ import { readCart} from '../addToCart/functions';
 function Cart() {
     let cartItems=readCart();
 
+    let [totalPrice, setTotalPrice]=useState('calculating');
+
+    useEffect(_=>{
+        (async ()=>{
+            console.log(cartItems);
+            let total=0;
+            for (let i=0; i<cartItems.length; i++) {
+                //Get price of item
+                let productUrl=`http://localhost:1028/api/products/${cartItems[i].id}`
+                let temp=await fetch(productUrl);
+                let json=await temp.json();
+                let price=json.price;
+
+                //Add item quantity times item price
+                total+=parseInt(cartItems[i].quantity)*price;
+            }
+            setTotalPrice(total);
+        })();
+    }, [cartItems]);
+
     return (
         <div className="max-w-[1500px] mx-auto my-6">
             <h1 className='text-center'>Shopping Cart</h1>
             <main className='flex max-w-[1500px] mx-auto justify-around'>
                 <div id='cart-left'>
-                    {console.log(readCart()) /*log cartItems*/}
                     {
                         cartItems.length>0 //Item
                         ?
@@ -23,8 +42,7 @@ function Cart() {
                     }
                 </div>
                 <div id='cart-right' className='border-solid border-2 border-gray-300 p-5 rounded-xl shadow-xl h-fit'>
-                    <p>Subtotal of 2 items</p>
-                    <p className='border-b-2 border-b-solid border-b-gray-300 pb-2 mb-4'>$4.99</p>
+                    <p>Subtotal of {cartItems.length} items</p>
                     <button>Proceed to checkout</button>
                 </div>
             </main>
